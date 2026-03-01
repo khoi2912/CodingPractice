da7ff4dea99bc774387598de797a49dfe805a771
khoi2912 <khoihoang2912@gmail.com>
2026-03-01 19:19:40 +0900

    update lessson 4

--- Khoi/Buoi4/main.c
+++ Khoi/Buoi4/main.c
@@ -4,16 +4,15 @@ int main() {
     ////// for ////////
 
     // Trường hợp 1 :
+    
     // for (int i = 0; i < 10; i ++){
     //     printf("i : %d\n", i);
     // }
    
-    
     // Trường hợp 2:
     // int i ;
-    // for ( i = 0 ; i < 10; i ++){
+    // for (i = 0 ; i < 20; i ++){
     //     printf("i trong vòng lặp : %d\n", i);
-        
     // }
 
     // printf("i ngoài vòng lặp: %d\n", i);
@@ -22,26 +21,74 @@ int main() {
 
     ////// while ////////
     // Trường hợp có thời hạn:
-     int i = 0;
-    while (i < 10)
-    {
-        printf("i : %d\n", i);
-        i = i + 1;
-    }
+    // int i = 0;
+    // while (i < 10)
+    // {
+    //     printf("i : %d\n", i); 
+    //     i = i + 1; 
+    // }
 
     // Trường hợp vô thời hạn:
-    int i = 15;
+    // int i = 20;
 
-    while (1)
-    {
-        i = i - 1; 
-        printf("i ở ngoài : %d\n", i); 
+    // while (1)
+    // {
+    //     i = i + 1; 
+    //     printf("i ở ngoài : %d\n", i); // 21 
+    //     if (i < 30){
+    //         printf("i ở trong : %d\n", i); // 21
+    //         break; // điều kiện để thoát vòng lặp
+    //     }   
+    // }
+ 
+    //////// do while ///////// 
+    // While != Do While
+    // While: check điều kiện trước rồi mới chạy 
+    // do while: chạy trước kiểm tra điều kiện sau
 
-        if (i < 15){
-            printf("i ở trong : %d\n", i);
-            break; // điều kiện để thoát vòng lặp
-        }   
+    // Ví dụ: Nhập số dương (bắt nhập lại nếu sai) -> dùng do-while
+    // Yêu cầu:
+    // Người dùng phải nhập số > 0
+    // Nếu nhập <=0 -> yêu cầu nhập lại
+    // Khi nhập đúng -> in ra số đó
+
+
+
+    /////
+    // int a;
+    // do {
+    //     printf( " Nhap:\n" );
+    //     scanf("%d", &a);
+    //     if (a <= 0 ) {
+    //         printf( "So khong lon hon 0\n" );
+    //         printf( "Nhap lai :\n" );
+    //     }
+    // } while (a <= 0 );
+    // printf( "So da nhap dung: %d \n", a );
+
+
+    // Ví dụ: Tính tổng các số từ 1 -> n
+    // Yêu cầu:
+    // Nhập số nguyên dương n
+    // Tính tổng
+    // S = 1 + 2 + 3 + ... + n
+    // in kết quả
+
+    int n;
+    int S = 0;
+    do {
+        printf("Nhap so duong n: ");
+        scanf("%d", &n); 
+
+        if(n <= 0) {
+            printf("So khong hop le, vui long nhap lai. \n");
+        }
+    }while (n <= 0);
+    
+    for (int i = 1; i <= n; i++) {
+        S = S + i;
     }
+    printf("S tu 1 den %d = %d\n",n, S);
 
-    return 0; 
+    return 0;
 }
