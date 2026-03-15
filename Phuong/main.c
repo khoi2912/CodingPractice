f5d49d62cd50c2226f91d507072493df7dff2e9a
phphuong1209 <ph.phuong111@gmail.com>
2026-03-15 16:33:46 +0900

    update Phuong for loop

--- Phuong/main.c
+++ Phuong/main.c
@@ -1,6 +1,21 @@
+// for co ban: tinh tong tu 1 den n
 #include <stdio.h>
 
 int main() {
-    printf("Hello Phuong 323232323");
+    int n, i = 1;
+    int sum = 0;  
+        
+    do
+    {
+        printf("Nhap n: ");
+        scanf("%d", &n);
+    } while (n <= 0);
+    for (int i = 1; i <= n; i++)
+    {
+        sum = sum + i; 
+        
+    }
+    printf("S = %d\n", sum);
     return 0;
+    
 }
