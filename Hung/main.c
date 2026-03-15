e1949a52f9bb2994ccc73923db73e590fd760d22
phihunghcm09-maker <phihunghcm09@gmail.com>
2026-03-15 14:28:58 +0700

    update Hung for loop

--- Hung/main.c
+++ Hung/main.c
@@ -1,6 +1,16 @@
+//BT1 : S=1+1/2+1/3+...+1/n
 #include <stdio.h>
 
-int main() {
-    printf("Hello Hung Tiger ");
+int main()
+{
+    int S = 0;
+    int i;
+    for(i = 1 ; i <= 6 ; i++)
+    {
+        S = S + i;
+    }
+
+    printf("S = %d", S);
+
     return 0;
 }
