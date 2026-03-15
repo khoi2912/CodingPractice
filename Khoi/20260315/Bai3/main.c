1cfeda748444a5696ec58b6bddbbb99b5899fe2a
khoi2912 <khoihoang2912@gmail.com>
2026-03-15 18:12:20 +0900

    Update 20260315 practice bai 4

--- Khoi/20260315/Bai3/main.c
+++ Khoi/20260315/Bai3/main.c
@@ -1,21 +1,3 @@
-// #include <stdio.h>
-
-// int main() {
-//     int n;
-//     printf("Nhap so co 2 chu so: ");
-//     scanf("%d", &n);
-
-//     int tens = n / 10;    
-//     int units = n % 10;   
-
-//     int reversed = units * 10 + tens;  
-
-//     printf("So dao nguoc: %d\n", reversed);
-
-//     return 0;
-// }
-
-// Cách 2:
 #include <stdio.h>
 
 int main() {
@@ -23,20 +5,38 @@ int main() {
     printf("Nhap so co 2 chu so: ");
     scanf("%d", &n);
 
-    if (n < 10 || n > 99) {
-        printf("Vui long nhap so co 2 chu so!\n");
-        return 1;
-    }
-
-    int tens = n / 10;   
+    int tens = n / 10;    
     int units = n % 10;   
 
-    int diff = units - tens;      
-    int sum = tens + units;       
-    int delta = diff * sum;       
-    int reversed = n + delta;     
+    int reversed = units * 10 + tens;  
 
     printf("So dao nguoc: %d\n", reversed);
 
     return 0;
 }
+
+// Cách 2:
+// #include <stdio.h>
+
+// int main() {
+//     int n;
+//     printf("Nhap so co 2 chu so: ");
+//     scanf("%d", &n);
+
+//     if (n < 10 || n > 99) {
+//         printf("Vui long nhap so co 2 chu so!\n");
+//         return 1;
+//     }
+
+//     int tens = n / 10;   
+//     int units = n % 10;   
+
+//     int diff = units - tens;      
+//     int sum = tens + units;       
+//     int delta = diff * sum;       
+//     int reversed = n + delta;     
+
+//     printf("So dao nguoc: %d\n", reversed);
+
+//     return 0;
+// }
--- Khoi/20260315/Bai4/main.c
+++ Khoi/20260315/Bai4/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+// for
 int main() {
     int n;
 
@@ -15,3 +16,47 @@ int main() {
 
     return 0;
 }
+
+// While
+// #include <stdio.h>
+
+// int main() {
+//     int n;
+//     printf("Nhap so hang n: ");
+//     scanf("%d", &n);
+
+//     int i = 1;  // hàng
+//     while (i <= n) {
+//         int j = 1;  // số trong hàng
+//         while (j <= i) {
+//             printf("%d ", j);
+//             j++;
+//         }
+//         printf("\n");
+//         i++;
+//     }
+
+//     return 0;
+// }
+
+// do-while
+// #include <stdio.h>
+
+// int main() {
+//     int n;
+//     printf("Nhap so hang n: ");
+//     scanf("%d", &n);
+
+//     int i = 1;
+//     do {
+//         int j = 1;
+//         do {
+//             printf("%d ", j);
+//             j++;
+//         } while (j <= i);
+//         printf("\n");
+//         i++;
+//     } while (i <= n);
+
+//     return 0;
+// }
