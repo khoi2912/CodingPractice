532a51a38c4aa8c404ea2336a68e8677fff5ce73
khoi2912 <khoihoang2912@gmail.com>
2026-03-15 18:09:53 +0900

    Update 20260315 practice bai 3

--- Khoi/20260315/Bai3/main.c
+++ Khoi/20260315/Bai3/main.c
@@ -1,3 +1,21 @@
+// #include <stdio.h>
+
+// int main() {
+//     int n;
+//     printf("Nhap so co 2 chu so: ");
+//     scanf("%d", &n);
+
+//     int tens = n / 10;    
+//     int units = n % 10;   
+
+//     int reversed = units * 10 + tens;  
+
+//     printf("So dao nguoc: %d\n", reversed);
+
+//     return 0;
+// }
+
+// Cách 2:
 #include <stdio.h>
 
 int main() {
@@ -5,10 +23,18 @@ int main() {
     printf("Nhap so co 2 chu so: ");
     scanf("%d", &n);
 
-    int tens = n / 10;    
+    if (n < 10 || n > 99) {
+        printf("Vui long nhap so co 2 chu so!\n");
+        return 1;
+    }
+
+    int tens = n / 10;   
     int units = n % 10;   
 
-    int reversed = units * 10 + tens;  
+    int diff = units - tens;      
+    int sum = tens + units;       
+    int delta = diff * sum;       
+    int reversed = n + delta;     
 
     printf("So dao nguoc: %d\n", reversed);
 
